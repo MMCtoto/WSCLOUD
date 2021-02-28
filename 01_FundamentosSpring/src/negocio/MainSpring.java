907db25598a5f7039095dcf443186fb254d1117c
MMCtoto <38754956+MMCtoto@users.noreply.github.com>
2021-02-28 14:27:49 +0100

    Add files via upload

---
+++ 01_FundamentosSpring/src/negocio/MainSpring.java
@@ -0,0 +1,23 @@
+package negocio;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import entidad.Casa;
+
+
+public class MainSpring {
+
+	public static ApplicationContext context = null;
+	
+	public static void main(String[] args) {
+
+		context = new ClassPathXmlApplicationContext("recursos/context.xml");
+		Casa casa = context.getBean("casa", Casa.class);
+		System.out.println(casa);
+		
+		}
+	}
+	
